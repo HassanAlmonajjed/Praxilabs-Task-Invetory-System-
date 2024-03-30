e77096f9347382d7ede072c9c77d6eb1a972a97d
Hassan <hassanalmonajjed@gmail.com>
2024-03-30 22:26:45 +0400

    Select and highlight items and put limit to number of selectable items

--- Assets/Scripts/Inventory Item/InventoryItemUI.cs
+++ Assets/Scripts/Inventory Item/InventoryItemUI.cs
@@ -1,21 +1,62 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 namespace PraxilabsTask
 {
     public class InventoryItemUI : MonoBehaviour
     {
-        [SerializeField] private Image _image;
+        [SerializeField] private Image _IconImage;
+        [SerializeField] private Image _frameImage;
+        [SerializeField] private Sprite _highlightFrame;
+        [SerializeField] private Sprite _normalFrame;
         [SerializeField] private TextMeshProUGUI _numberOfItems;
+
         private Button _button;
+        public bool IsSelected { get; private set; }
+        private InventoryUI _inventoryUI;
+        public InventoryItem InventoryItem { get; set; }
+
+        public event Action<InventoryItemUI> OnItemSelected;
 
         private void Awake()
         {
             _button = GetComponent<Button>();
+            _inventoryUI = GetComponentInParent<InventoryUI>();
+            _button.onClick.AddListener(OnItemClicked);
         }
 
-        public void SetImage(Sprite icon) => _image.sprite = icon;
-        public void SetNumberOfItems(int numberOfItems) => _numberOfItems.SetText(numberOfItems.ToString());
+        public void Setup(InventoryItem inventoryItem, int numberOfItems)
+        {
+            _IconImage.sprite = inventoryItem.Icon;
+            _numberOfItems.SetText(numberOfItems.ToString());
+            InventoryItem = inventoryItem;
+        }
+
+        private void OnItemClicked() => OnItemSelected?.Invoke(this);
+
+        public void Toggle()
+        {
+            if (IsSelected)
+                UnSelect();
+            else
+                Select();
+        }
+
+        private void Select()
+        {
+            IsSelected = true;
+            Highlight();
+        }
+
+        private void UnSelect()
+        {
+            IsSelected = false;
+            UnHighlight();
+        }
+
+        private void Highlight() => _frameImage.sprite = _highlightFrame;
+        private void UnHighlight() => _frameImage.sprite = _normalFrame;
     }
 }
--- Assets/Scripts/Inventory/InventoryUI.cs
+++ Assets/Scripts/Inventory/InventoryUI.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace PraxilabsTask
 {
@@ -7,16 +8,50 @@ namespace PraxilabsTask
         [SerializeField] private Inventory _inventory;
         [SerializeField] private InventoryItemUI _inventoryItemUIPrefab;
         [SerializeField] private Transform _inventoryItemContainer;
+        [SerializeField] private int _maxSelectableItems = 4;
+
+        private readonly List<InventoryItem> _selectedItems = new();
 
         private void Start()
+        {
+            InitializeInventoryMenu();
+        }
+
+        private void InitializeInventoryMenu()
         {
             _inventory.CopyStartItems();
             foreach (var item in _inventory.AvailableItems)
             {
                 var inventoryItemUI = Instantiate(_inventoryItemUIPrefab, _inventoryItemContainer);
-                inventoryItemUI.SetImage(item.Key.Icon);
-                inventoryItemUI.SetNumberOfItems(item.Value);
+                inventoryItemUI.Setup(item.Key, item.Value);
+                inventoryItemUI.OnItemSelected += ItemClicked;
             }
+        }
+
+        public void ItemClicked(InventoryItemUI inventoryItemUI)
+        {
+            if (inventoryItemUI.IsSelected)
+            {
+                _selectedItems.Remove(inventoryItemUI.InventoryItem);
+                inventoryItemUI.Toggle();
+            }
+            else
+            {
+                if (_selectedItems.Count >= _maxSelectableItems)
+                {
+                    Debug.Log("Max possible items reached");
+                    return;
+                }
+
+                _selectedItems.Add(inventoryItemUI.InventoryItem);
+                inventoryItemUI.Toggle();
+            }
+            
+        }
+
+        public void ItemUnSelected(InventoryItem inventoryItem)
+        {
+
         }
     }
 }
